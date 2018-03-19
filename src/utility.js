97bdda1535a0a5f615926426eae4913daaed09f7
Yash Parihar <pariharyash6@gmail.com>
2018-03-19 19:18:42 +0530

    sds

---
+++ src/utility.js
@@ -0,0 +1,48 @@
+
+var dbPromise = idb.open('actionurl-store', 2, function (db) {
+  if (!db.objectStoreNames.contains('actionurl')) {
+    db.createObjectStore('actionurl', {keyPath: 'id'});
+  }
+});
+
+function writeData(st, data) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.put(data);
+      return tx.complete;
+    });
+}
+
+function readAllData(st) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readonly');
+      var store = tx.objectStore(st);
+      return store.getAll();
+    });
+}
+
+function clearAllData(st) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.clear();
+      return tx.complete;
+    });
+}
+
+function deleteItemFromData(st, id) {
+  dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.delete(id);
+      return tx.complete;
+    })
+    .then(function() {
+      console.log('Item deleted!');
+    });
+}
